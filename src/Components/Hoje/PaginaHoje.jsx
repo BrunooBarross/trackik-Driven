28e2b81df85379807e8612744157e186d584a16c
BrunooBarross <barrosbrunofelipe@gmail.com>
2022-04-01 20:38:56 -0300

    feat: adicionando ternário de mensagem na falta de hábitos do dia

--- src/Components/Hoje/PaginaHoje.jsx
+++ src/Components/Hoje/PaginaHoje.jsx
@@ -33,25 +33,32 @@ const PaginaHoje = () =>{
     return (
         <Container>
             <Header />
-                <Titulo>
-                    <h1>{dayjs().locale('pt-br').format('dddd, DD/MM')}</h1>
-                    <h2>Nenhum hábito concluído ainda</h2>
-                </Titulo>
-
-                <HabitosHoje>
-                    {listarHoje.map((item, key) =>
-                        <ListaHoje
-                            key={key}
-                            id={item.id}
-                            name={item.name}
-                            done={item.done}
-                            sequencia={item.currentSequence}
-                            recorde={item.highestSequence}
-                            token={token}
-                            monitoraCheck={monitoraCheck}
-                            setMonitoraCheck={setMonitoraCheck}
-                        />)}
-                </HabitosHoje>
+                { listarHoje.length ===0 ?  
+                    <Aviso>
+                        <span>Você não tem nenhum hábito para realizar hoje!</span>
+                    </Aviso> 
+                    :
+                    <>
+                        <Titulo>
+                            <h1>{dayjs().locale('pt-br').format('dddd, DD/MM')}</h1>
+                            <h2>Nenhum hábito concluído ainda</h2>
+                        </Titulo>
+                        <HabitosHoje>
+                            {listarHoje.map((item, key) =>
+                                <ListaHoje
+                                    key={key}
+                                    id={item.id}
+                                    name={item.name}
+                                    done={item.done}
+                                    sequencia={item.currentSequence}
+                                    recorde={item.highestSequence}
+                                    token={token}
+                                    monitoraCheck={monitoraCheck}
+                                    setMonitoraCheck={setMonitoraCheck}
+                                />)}
+                        </HabitosHoje>
+                    </>     
+                }
             <Footer />
         </Container>
         
@@ -78,4 +85,14 @@ const Titulo = styled.div`
 const HabitosHoje = styled.div`
    width: 89%;
    margin-bottom: 95px;
+`
+const Aviso = styled.div`
+    width: 90%;
+    margin-top: 29px;
+
+    span{
+        font-size: 17.976px;
+        line-height: 22px;
+        color: #666666;
+    }
 `
